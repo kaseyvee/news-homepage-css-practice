d5d3562e16d450a814b0b16943490472eac91e9d
kethnal <kaseyvaldez@hotmail.com>
2023-03-23 20:44:56 -0700

    Move assets and start navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,26 @@
 function Nav(props) {
   return (
-    <div>
-      hello
+    <div className="nav">
+
+      <div className="nav-mobile-closed">
+        <img src="/assets/logo.svg" alt="logo"/>
+        <img src="/assets/icon-menu.svg" alt="menu icon"/>
+      </div>
+
+      <div className="nav-mobile-open">
+        <img src="/assets/icon-menu-close.svg" alt="close menu icon"/>
+        <ul>
+          <li>Home</li>
+          <li>New</li>
+          <li>Popular</li>
+          <li>Trending</li>
+          <li>Categories</li>
+        </ul>
+      </div>
+
+      <div className="nav-desktop">
+      </div>
+
     </div>
   );
 }
