3625f9c6c8cfc032c0696b97fa7d65ee596bb319
kethnal <kaseyvaldez@hotmail.com>
2023-04-03 19:44:23 -0700

    create featured article mark up

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -2,8 +2,21 @@ import React from 'react';
 
 function Featured(props) {
   return (
-    <div>
-      hello
+    <div className='featured'>
+      <div className='featured_image-container'>
+        <img src='/assets/image-web-3-desktop.jpg' />
+      </div>
+
+      <h1 className='featured_title'>
+        The Bright Future of Web 3.0?
+      </h1>
+
+      <div className='featured_article-preview'>
+        <p className='featured_article-preview_content'>
+          We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it fulfilling its promise?
+        </p>
+        <button className='featured_article-preview_button'>READ MORE</button>
+      </div>
     </div>
   );
 }
