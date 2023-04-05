9934c70eaec953975d3e7d2698c88c35fc661619
kethnal <kaseyvaldez@hotmail.com>
2023-04-04 17:54:07 -0700

    Add mobile nav animation and edit grid spacing

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,18 +2,18 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 function Nav() {
+  const [mobileNavAnimation, setMobileNavAnimation] = useState("");
   const [openNav, setOpenNav] = useState(false);
 
-  useEffect(() => {
-    setOpenNav(false);
-  }, [])
-
-  function handleToggleNav() {
-    if (openNav) {
-      return setOpenNav(false);
+  function handleNavToggle() {
+    if (mobileNavAnimation === "slideIn") {
+      setMobileNavAnimation("slideOut");
+      return setTimeout(() => {
+        setOpenNav(false);
+      }, 300)
     }
-
-    return setOpenNav(true);
+    setOpenNav(true);
+    return setMobileNavAnimation("slideIn");
   }
   
   return (
@@ -25,7 +25,7 @@ function Nav() {
           src="icon-menu.svg"
           alt="menu icon"
           className="menu-icon"
-          onClick={handleToggleNav}
+          onClick={handleNavToggle}
         />
         <div className="nav-items">
           <div className="nav-item">Home</div>
@@ -36,19 +36,24 @@ function Nav() {
         </div>
       </div>
 
-      { openNav && <div className="nav-mobile-open">
-        <div className="nav-mobile-open-overlay"></div>
-        <div className="nav-mobile-open-main">
+      {openNav && <div className="nav-mobile-open">
+        {mobileNavAnimation === "slideIn" && <div className="nav-mobile-open-overlay"></div>}
+        <div
+          className="nav-mobile-open-main"
+          style={{
+            animation: `${mobileNavAnimation} 300ms ease-in-out forwards`
+          }}
+        >
           <img
             src="icon-menu-close.svg"
             alt="close menu icon"
             className="close-menu-icon"
-            onClick={handleToggleNav}
+            onClick={handleNavToggle}
           />
           <div className="nav-items">
             <div className="nav-item">Home</div>
             <div className="nav-item">New</div>
-            <div className="nav-item"> Popular</div>
+            <div className="nav-item">Popular</div>
             <div className="nav-item">Trending</div>
             <div className="nav-item">Categories</div>
           </div>
